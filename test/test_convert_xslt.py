055fa59f47bbbe3a2fee252075baf3635bd13c35
Jaromir Hradilek <jhradilek@gmail.com>
2024-11-20 23:37:01 +0100

    Added tests for the dita.convert.xslt module.

---
+++ test/test_convert_xslt.py
@@ -0,0 +1,31 @@
+import unittest
+from pathlib import PurePath
+from dita.convert import xslt
+
+class TestDitaConvertXSLT(unittest.TestCase):
+    def test_concept_returns_path(self):
+        self.assertTrue(isinstance(xslt.concept, PurePath))
+
+    def test_concept_returns_file(self):
+        self.assertTrue(xslt.concept.is_file())
+
+    def test_reference_returns_path(self):
+        self.assertTrue(isinstance(xslt.reference, PurePath))
+
+    def test_reference_returns_file(self):
+        self.assertTrue(xslt.reference.is_file())
+
+    def test_task_returns_path(self):
+        self.assertTrue(isinstance(xslt.task, PurePath))
+
+    def test_task_returns_file(self):
+        self.assertTrue(xslt.task.is_file())
+
+    def test_task_generated_returns_path(self):
+        self.assertTrue(isinstance(xslt.task_generated, PurePath))
+
+    def test_task_generated_returns_file(self):
+        self.assertTrue(xslt.task_generated.is_file())
+
+if __name__ == "__main__":
+    unittest.main()
