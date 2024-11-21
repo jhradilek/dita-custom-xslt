67a7a5a35eb2a835c6c6dda38b7a4c9199e1ff9c
Jaromir Hradilek <jhradilek@gmail.com>
2024-11-21 01:49:03 +0100

    Added more tests for the dita.convert.transform module.

--- test/test_convert_transform.py
+++ test/test_convert_transform.py
@@ -37,7 +37,7 @@ class TestDitaConvertTransform(unittest.TestCase):
     def test_to_reference_requires_topic(self):
         xml = etree.parse(StringIO('''\
         <reference id="example-concept">
-            <title>Concept title</title>
+            <title>Reference title</title>
         </reference>
         '''))
 
@@ -68,7 +68,7 @@ class TestDitaConvertTransform(unittest.TestCase):
     def test_to_task_requires_topic(self):
         xml = etree.parse(StringIO('''\
         <task id="example-concept">
-            <title>Concept title</title>
+            <title>Task title</title>
         </task>
         '''))
 
@@ -77,10 +77,29 @@ class TestDitaConvertTransform(unittest.TestCase):
 
         self.assertEqual(str(cm.exception), 'ERROR: Not a DITA topic')
 
+    def test_to_task_forbids_sections(self):
+        xml = etree.parse(StringIO('''\
+        <topic id="example-topic">
+            <title>Topic title</title>
+            <body>
+                <p>Topic introduction</p>
+                <section>
+                    <title>Section title</title>
+                    <p>Section body</p>
+                </section>
+            </body>
+        </topic>
+        '''))
+
+        with self.assertRaises(etree.XSLTApplyError) as cm:
+            transform.to_task(xml)
+
+        self.assertEqual(str(cm.exception), 'ERROR: Section not allowed in a DITA task')
+
     def test_to_task_generated_requires_topic(self):
         xml = etree.parse(StringIO('''\
         <task id="example-concept">
-            <title>Concept title</title>
+            <title>Task title</title>
         </task>
         '''))
 
@@ -88,3 +107,22 @@ class TestDitaConvertTransform(unittest.TestCase):
             transform.to_task_generated(xml)
 
         self.assertEqual(str(cm.exception), 'ERROR: Not a DITA topic')
+
+    def test_to_task_generated_forbids_sections(self):
+        xml = etree.parse(StringIO('''\
+        <topic id="example-topic">
+            <title>Topic title</title>
+            <body>
+                <p>Topic introduction</p>
+                <section>
+                    <title>Section title</title>
+                    <p>Section body</p>
+                </section>
+            </body>
+        </topic>
+        '''))
+
+        with self.assertRaises(etree.XSLTApplyError) as cm:
+            transform.to_task_generated(xml)
+
+        self.assertEqual(str(cm.exception), 'ERROR: Section not allowed in a DITA task')
